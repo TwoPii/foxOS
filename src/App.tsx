1b8bb2811e381fb83e7be52e41e089e88eb4dce7
Genís Bosch <genisb8@rocketmail.com>
2025-08-28 00:52:05 +0200

    Fox background added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import styles from "./App.module.scss";
 import Programs from "./components/programs/Programs";
+import Fox from "./assets/icons/fox.svg?react";
 
 const App = () => {
   return (
     <div className={styles.container}>
+      <Fox className={styles.foxBackground}></Fox>
       <Programs />
     </div>
   );
